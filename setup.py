dd339517367173421b6fe89dec3c2da964450a51
Larry Orton <orton.larry84@gmail.com>
2024-11-20 20:11:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name="WireWolf",
+    version="1.0.0",
+    author="Larry Orton",
+    author_email="larry.orton@berkeley.edu",
+    description="A powerful network scanning tool for cybersecurity professionals.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "python-nmap",
+        "requests",
+        "geoip2",
+        "ipwhois"
+    ],
+    entry_points={
+        "console_scripts": [
+            "WireWolf=WireWolf.scanner:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
